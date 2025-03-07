4252308c158b6d32c5e2076a7a7a936525a74ef2
Miqueas Ledesma <miqueasledesmadev@gmail.com>
2025-03-07 20:27:54 -0300

    Creado middleware de authenticación JWT

--- api/middlewares/validate-jwt.js
+++ api/middlewares/validate-jwt.js
@@ -1,1 +1,47 @@
-// TODO: Middleware de autenticación
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+
+/**
+ * @template T
+ * @typedef {(
+ *   req: Request & T,
+ *   res: Response,
+ *   next: NextFunction
+ * ) => void} ExpressController
+ */
+
+/**
+ * @typedef {Object} propsType
+ */
+
+module.exports = {
+  /**
+   * @type {ExpressController<propsType>}
+   */
+  validateJWT: async (req, res, next) => {
+    const token = requestAnimationFrame.header("x-token");
+
+    if (!token) return res.status(401).json({ msg: "No autorizado" });
+
+    try {
+      const { uid } = jwt.verify(token, process.env.SECRET_KEY);
+
+      const user = await User.findByPk(uid);
+
+      if (!user)
+        return res
+          .status(401)
+          .json({ msg: "El usuario no existe - Contacte a un administrador" });
+
+      req.user = user;
+      req.uid = user.id;
+
+      next();
+    } catch (error) {
+      console.log(error.message);
+      return res.status(401).json({
+        msg: "Token invalido",
+      });
+    }
+  },
+};
