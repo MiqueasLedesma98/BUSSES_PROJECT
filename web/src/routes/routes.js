af3b46444e42752bcf6ccde66fff09190ae6b527
Miqueas Ledesma <miqueasledesmadev@gmail.com>
2025-03-14 07:58:38 -0300

    cambio en tipado de las rutas

--- web/src/routes/routes.js
+++ web/src/routes/routes.js
@@ -1,36 +1,36 @@
 import { lazy } from "react";
 
 /**
- * @type {[import('react-router-dom').RouteObject]}
+ * @type {Array<import('react-router-dom').RouteObject>}
  */
 const routes = [
   {
     path: "/",
-    component: lazy(() => import("../pages/Login")),
+    element: lazy(() => import("../pages/Login")),
   },
   {
     path: "/dashboard",
-    component: lazy(() => import("../pages/Dashboard")),
+    element: lazy(() => import("../pages/Dashboard")),
   },
   {
     path: "/dashboard/movies",
-    component: lazy(() => import("../pages/Movies")),
+    element: lazy(() => import("../pages/Movies")),
   },
   {
     path: "/dashboard/musics",
-    component: lazy(() => import("../pages/Musics")),
+    element: lazy(() => import("../pages/Musics")),
   },
   {
     path: "/dashboard/advertising",
-    component: lazy(() => import("../pages/Construction")),
+    element: lazy(() => import("../pages/Construction")),
   },
   {
     path: "/dashboard/enterprise",
-    component: lazy(() => import("../pages/Construction")),
+    element: lazy(() => import("../pages/Construction")),
   },
   {
     path: "/dashboard/metrics",
-    component: lazy(() => import("../pages/Construction")),
+    element: lazy(() => import("../pages/Construction")),
   },
 ];
 
