041a71b9b3b0ca079db87db2809801c4f0f11672
Miqueas Ledesma <miqueasledesmadev@gmail.com>
2025-07-28 22:04:09 -0300

    ecosystem.config changes

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
       name: "Veotrans",
       watch: true,
       script: "./bin/www",
-      instances: process.env.NODE_ENV === "MAIN_SERVER" ? 30 : "max",
+      instances: 20,
       exec_mode: "cluster",
       autorestart: true,
       env: {
         PORT: 4050,
+        NODE_ENV: "MAIN_SERVER",
       },
     },
   ],
