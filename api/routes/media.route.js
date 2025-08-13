d1e80cae14cfe2aeda053ef0632ced6092630be1
Miqueas Ledesma <miqueasledesmadev@gmail.com>
2025-08-12 21:32:19 -0300

    FIX: media music validation

--- api/routes/media.route.js
+++ api/routes/media.route.js
@@ -7,7 +7,12 @@ const { validateFields } = require("../middlewares");
 router.get(
   "/:type/:lang/:name",
   [
-    check("type", "No es un typo válido").isIn(["banner", "movie", "video"]),
+    check("type", "No es un typo válido").isIn([
+      "banner",
+      "movie",
+      "video",
+      "music",
+    ]),
     check("lang", "No es un lenguaje válido").isIn(["esp", "eng"]),
     check("name", "Name es obligatorio").notEmpty(),
     validateFields,
